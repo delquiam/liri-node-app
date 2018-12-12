52a72a8c4d51f6695e91664e29d03b69f4452a32
wonder woman <rdelquiam@gmail.com>
2018-12-12 01:07:02 -0800

    omdb working

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
 console.log('this is loaded');
 
-exports.spotify = {
-    id:  process.env.SPOTIFY_ID,
-    secret:  process.env.SPOTIFY_SECRET
+exports.spotifyKeys = {
+    // id:  process.env.SPOTIFY_ID,
+    // secret:  process.env.SPOTIFY_SECRET
+    SPOTIFY_ID:  '59fdd873e66a484198cd0cd1c96ad3b4',
+
+SPOTIFY_SECRET:  '54e0395711e248a1b57163b98a8a6461',
+
 };
--- liri.js
+++ liri.js
@@ -4,9 +4,11 @@ var fs = require("fs");// Core node package for reading and writing files
 var inquirer = require("inquirer");
 var axios = require("axios");
 var moment = require('moment'); moment().format();
-var spotify = require("node-spotify-api");
+var spotifyNode = require("node-spotify-api");
 var request = require("request");
-
+var command = process.argv;
+var action = command[2];
+var input = command[3];
 
 // function startLiri() {
 //     inquirer.prompt([
@@ -21,25 +23,25 @@ var request = require("request");
 //             ]
 //         }
 //     ]).then(function (response) {
-var pick = function (caseData, functionData) {
-    switch (caseData) {
-        case `concert-this`:
-           concertThis(functionData);
-            break;
-        case `spotify-this-song`:
-            spotifySongs(functionData);
-            break;
-        case `movie-this`:
-            omdbThis(functionData);
-            break;
-        case `do-what-it-says`:
-            doWhatItSays(functionData);
-            break;
-        default:
-            console.log('LIRI does not know that');
-    }
-    // });
-}
+
+switch (action) {
+    // case `concert-this`:
+    //     concertThis(input);
+    //     break;
+    case `spotify-this-song`:
+        spotifySongs(input);
+        break;
+    case `movie-this`:
+        omdbThis(input);
+        break;
+    case `do-what-it-says`:
+        doWhatItSays();
+        break;
+    default:
+        console.log('Sorry, LIRI does not know that command!');
+};
+
+
 
 // function callBandsInTownAPI() {
 //     inquirer.prompt([
@@ -49,31 +51,31 @@ var pick = function (caseData, functionData) {
 //             message: "What artist?"
 //         }])
 //         .then(function (response) {
-            // var bandsInTownURL = "https://rest.bandsintown.com/artists/" + response.artist + "/events?app_id=codingbootcamp"
-            // //             // var concerts = axiosGet(bandsInTownURL);
-            // //             // console.log(concerts);
+// var bandsInTownURL = "https://rest.bandsintown.com/artists/" + response.artist + "/events?app_id=codingbootcamp"
+// //             // var concerts = axiosGet(bandsInTownURL);
+// //             // console.log(concerts);
 
-            // axios.get(bandsInTownURL)
-            //     .then(function (response) {
-            //         var data = response.data;
-            //         console.log(data);
+// axios.get(bandsInTownURL)
+//     .then(function (response) {
+//         var data = response.data;
+//         console.log(data);
 
-            var concertThis = function (concertName) {
-                // //             // var concerts = axiosGet(bandsInTownURL);
-                request("https://rest.bandsintown.com/artists/" + concertName + "/events?app_id=codingbootcamp", function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        for (var i = 0; i < data.length; i++) {
-                            console.log("Venue: " + data[i].venue.name);
-                            console.log("Country: " + data[i].venue.country);
-                            console.log("City: " + data[i].venue.city);
-                            console.log("Date: " + data[i].datetime);
-                        }
-                      }
-                    })
-                }
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
+// var concertThis = function (concertName) {
+//     // //             // var concerts = axiosGet(bandsInTownURL);
+//     request("https://rest.bandsintown.com/artists/" + concertName + "/events?app_id=codingbootcamp", function (error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             for (var i = 0; i < data.length; i++) {
+//                 console.log("Venue: " + data[i].venue.name);
+//                 console.log("Country: " + data[i].venue.country);
+//                 console.log("City: " + data[i].venue.city);
+//                 console.log("Date: " + data[i].datetime);
+//             }
+//         }
+//     })
+// }
+// .catch(function (error) {
+//     console.log(error);
+// });
 
 
 // function axiosGet(url) {
@@ -89,64 +91,71 @@ var pick = function (caseData, functionData) {
 
 
 //------------------------------------------------------------------------------------------------------//
+// function spotifySongs(input){
+// // var spotify = new Spotify(keys.spotify);
 
-// var spotify = new spotify(keys.spotify);
-// var artistsNames = function (artist) {
-//     return artist.name;
+// if (!input) {
+//     input = "The Sign";
 // }
-// var spotifySongs = function (songName) {
-//     spotify.search({ type: 'track', query: songName }, function (err, data) {
+
+// // var artistsNames = function (artist) {
+// //     return artist.name;
+// // }
+
+//     spotify.search({ type: 'track', query: input }, function (err, data) {
 //         if (err) {
 //             console.log('Error occurred: ' + err);
 //             return;
 //         }
 //         var songInfo = data.tracks.items;
-//         for (var i = 0; i < songInfo.length; i++) {
-//             console.log[i];
-//             console.log('Artist(s):  ' + songInfo[i].artists.map(artistsNames));
-//             console.log('Song:  ' + songInfo[i].name);
-//             console.log('Preview Link:  ' + songInfo[i].preview_url);
-//             console.log('Album:  ' + songInfo[i].album.name);
+//         // for (var i = 0; i < songInfo.length; i++) {
+//             // console.log[i];
+//             console.log('Artist(s):  ' + songInfo[0].artists[0].name);
+//             console.log('Song:  ' + songInfo[0].name);
+//             console.log('Preview Link:  ' + songInfo[0].preview_url);
+//             console.log('Album:  ' + songInfo[0].album.name);
 //             console.log('----------------------------------------------');
-//         }
-//     });
-// }
+//         });
+//     }
+
+
+
 // //----------------------------------------------------------------------------------------------------//
 
-// var omdbThis = function (movieName) {
-//     request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             var jsonData = JSON.parse(body);
-//             console.log('Title:  ' + jsonData.Title);
-//             console.log('Year:  ' + jsonData.Year);
-//             console.log('IMDB Rating:  ' + jsonData.imdbRating);
-//             console.log('Rotten Tomatoes Rating:  ' + jsonData.tomatoRating);
-//             console.log('Country:  ' + jsonData.Country);
-//             console.log('Language:  ' + jsonData.Language);
-//             console.log('Plot:  ' + jsonData.Plot);
-//             console.log('Actors:  ' + jsonData.Actors);
-//             console.log('----------------------------------------------');
-//         }
-//     });
-// }
+function omdbThis(input) {
+    request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        if (!input) {
+            input = 'Mr Nobody';
+        }
+        if (!error && response.statusCode === 200) {
+            var jsonData = JSON.parse(body);
+            console.log('Title:  ' + jsonData.Title);
+            console.log('Year:  ' + jsonData.Year);
+            console.log('IMDB Rating:  ' + jsonData.imdbRating);
+            console.log('Rotten Tomatoes Rating:  ' + jsonData.tomatoRating);
+            console.log('Country:  ' + jsonData.Country);
+            console.log('Language:  ' + jsonData.Language);
+            console.log('Plot:  ' + jsonData.Plot);
+            console.log('Actors:  ' + jsonData.Actors);
+            console.log('----------------------------------------------');
+        }
+    });
+};
 // //-------------------------------------------------------------------------------------------//
 
-// var doWhatItSays = function () {
-//     fs.readFile("random.txt", "utf8", function (error, data) {
-//         if (error) {
-//             return console.log(error);
-//         }
+function doWhatItSays () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
 
-//         var dataArr = data.split(",");
+        var dataArr = data.split(",");
 
-//         if (dataArr.length == 2) {
-//             pick(dataArr[0], dataArr[1]);
-//         }
-//         else if (dataArr.length == 1);
-//         pick(dataArr[0]);
-//     } 
-// });
-
-
-
-
+        if (dataArr.length == 2) {
+            pick(dataArr[0], dataArr[1]);
+        }
+        else if (dataArr.length == 1); {
+            pick(dataArr[0]);
+        }
+    });
+};
